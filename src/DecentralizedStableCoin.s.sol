3d94bda7ff5111796a3ad696ec05dad7f8c34585
talha073 <talhajavedd@gmail.com>
2023-08-16 21:31:24 +0400

    DSC implimentation

--- src/DecentralizedStableCoin.s.sol
+++ src/DecentralizedStableCoin.s.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title DecentralizedStableCoin
@@ -11,6 +13,8 @@ pragma solidity ^0.8.18;
  * This is the contract meant to be governed by DSCEngie. This contract is just the ERC20 implimentation of our satable coin system.
  */
 
-contract DecentralizedStableCoin {
-    
+contract DecentralizedStableCoin is ERC20Burnable, Ownable{
+    constructor () ERC20("DecentralizedStableCoin", "DSC") {
+        
+    }
 }
