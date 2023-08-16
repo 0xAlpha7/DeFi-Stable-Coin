53c3c53a8a347a9192b33571aac7fd6842037435
talha073 <talhajavedd@gmail.com>
2023-08-16 21:12:58 +0400

    contract setup and description

---
+++ src/DecentralizedStableCoin.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Talha
+ * Collateral: Exogenous (Eth and Btc)
+ * Minting: Algorithimic
+ * Relative Stability: Pegged to USD
+ * This is the contract meant to be governed by DSCEngie. This contract is just the ERC20 implimentation of our satable coin system.
+ */
+
+contract DecentralizedStableCoin {
+    
+}
