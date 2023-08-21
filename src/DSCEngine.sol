6bfa9ad2dad409c7fd9d2791eb4c38fae10af476
talha073 <talhajavedd@gmail.com>
2023-08-21 21:50:59 +0400

    functions setups

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -12,12 +12,27 @@ pragma solidity ^0.8.19;
 * - Algorithimic Stable
 *
 * This is similar to DAI. If DAI had no governance, no fees, and was only backed by WETH and WBTC
-
+*
+* Our DSC system should always be "overcollateraized". At no point, should the value of all collateral <= the $ backed value of all the DSC. 
+*
 * @notice: This contract is the core of the DCS System. It handles all the logics for minting and redeeming DCS, as well as depositing and withdrawing collateral. 
 * @notice: This contract is very loosly based on the makerDAO (DAI) system
 */
 contract DSCEngine {
-    constructor() {
+    
+    function depositeCollateralAndMintDSC() external {}
+
+    function redeemCollateralForDSC() external {}
+
+    function burnDSC() external {}
+
+    function liquidate() external {
+        //$100 ETH  --> $40 (liquidated) $60 --> kickout from the system because you are too close
+        //$50 DSC
+    }
+
+    function getHealthFactor() external {
+
         
     }
 }
