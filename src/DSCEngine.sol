f570d777858be14dfba76e825ba6e8544d49e26d
talha073 <talhajavedd@gmail.com>
2023-08-23 21:38:25 +0400

    deposite collateral implimentation

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
@@ -22,41 +23,42 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.
 * @notice: This contract is very loosly based on the makerDAO (DAI) system
 */
 contract DSCEngine is ReentrancyGuard {
-
     //!errors
-    error DSCEngine__NeedsMoreThanZero(); 
-    error DSCEngine__TokenAddressAndPriceFeedAddressMustBeSameLength(); 
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressAndPriceFeedAddressMustBeSameLength();
     error DSCEngine__NotAllowedToken();
 
     //!state variables
-    mapping (address token => address priceFeed) private s_priceFeed;  //token => priceFeed
-    mapping (address user => mapping(address token => uint256 amount)) private s_collateralDeposited ;
+    mapping(address token => address priceFeed) private s_priceFeed; //token => priceFeed
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
     DecentralizedStableCoin private immutable i_dsc;
+
+    //!events
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount); 
 
     //!modifiers
     modifier moreThanZero(uint256 amount) {
         // require((amount > uint256(0)), "Amount must not be zero");
-        if(amount == 0){
+        if (amount == 0) {
             revert DSCEngine__NeedsMoreThanZero();
         }
         _;
     }
+
     modifier isAllowedToken(address token) {
-        if(s_priceFeed[token] == address(0)){
+        if (s_priceFeed[token] == address(0)) {
             revert DSCEngine__NotAllowedToken();
         }
         _;
     }
-    
+
     //!functions
-    constructor (
-        address[] memory tokenAddress, address[] memory priceFeedAddress, address dscAddress
-    ) {
+    constructor(address[] memory tokenAddress, address[] memory priceFeedAddress, address dscAddress) {
         //USD priceFeed
-        if(tokenAddress.length != priceFeedAddress.length){
+        if (tokenAddress.length != priceFeedAddress.length) {
             revert DSCEngine__TokenAddressAndPriceFeedAddressMustBeSameLength();
         }
-        for(uint256 i = 0; i < tokenAddress.length; i++){
+        for (uint256 i = 0; i < tokenAddress.length; i++) {
             s_priceFeed[tokenAddress[i]] = priceFeedAddress[i];
         }
         i_dsc = DecentralizedStableCoin(dscAddress);
@@ -65,15 +67,18 @@ contract DSCEngine is ReentrancyGuard {
     //!external functions
     function depositeCollateralAndMintDsc() external {}
 
-
     /**
      * @param tokenCollateralAddress The address of the token todeposit as collateral
      * @param amountCollateral The amount of collateral to deposit
      */
-    function depositeCollateral(address tokenCollateralAddress, uint256 amountCollateral) external moreThanZero(amountCollateral) isAllowedToken(tokenCollateralAddress) nonReentrant 
+    function depositeCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
     {
         s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
-
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral); 
     }
 
     function redeemCollateralForDsc() external {}
