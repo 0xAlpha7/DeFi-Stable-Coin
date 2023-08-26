2cba249bf50c4467dbbc38fb6c38dfa01e488a07
talha073 <talhajavedd@gmail.com>
2023-08-26 16:25:41 +0400

    getAccountCollateralValue implimentation

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -152,12 +152,12 @@ contract DSCEngine is ReentrancyGuard {
 
      //!public and external, view functions
 
-     function getAccountCollateralValue(address user) public view returns (uint256) {
+     function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
         //loop throw each collateral token, get the amount they have deposited and map it to the price, to get the USD value
         for(uint256 i = 0; i < s_collateralTokens.length; i++){
             address token = s_collateralTokens[i];
             uint256 amount = s_collateralDeposited[user][token];
-            totalCollateralValueInUsd += 
+            totalCollateralValueInUsd += getUsdValue(token, amount);
         }
      }
 
