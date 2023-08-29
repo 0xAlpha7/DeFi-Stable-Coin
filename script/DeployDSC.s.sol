8de0812d891be08c287532467f0e52b4f511a4a7
talha073 <talhajavedd@gmail.com>
2023-08-29 22:04:30 +0400

    interaction with helperconfig

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -5,9 +5,20 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+
     function run() external returns(DecentralizedStableCoin, DSCEngine){
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcPriceFeed];
+
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
         // DSCEngine engine = new DSCEngine();
