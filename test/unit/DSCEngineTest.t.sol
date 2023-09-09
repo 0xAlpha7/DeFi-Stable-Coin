0a899209c566999fab3955f240bfe4753dce420f
talha073 <talhajavedd@gmail.com>
2023-09-09 17:32:00 +0400

    remove setu bug

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -30,10 +30,7 @@ contract DSCEngineTest is Test {
         (dsc, dsce, config) = deployer.run(); 
         (ethUsdPriceFeed, btcUsdPriceFeed , weth, , ) = config.activeNetworkConfig();
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
-         vm.startPrank(USER);
-        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
-        dsce.depositeCollateralAndMintDsc(weth, AMOUNT_COLLATERAL, AMOUNT_TO_MINT);
-        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+       
     }
 
     //TODO: Modifiers
