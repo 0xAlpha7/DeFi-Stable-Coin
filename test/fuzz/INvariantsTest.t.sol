477e99a419db892c3ff4919c97fa05a81a6c547b
talha073 <talhajavedd@gmail.com>
2023-09-21 20:58:51 +0400

    invariant setup

--- test/fuzz/INvariantsTest.t.sol
+++ test/fuzz/INvariantsTest.t.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 // Have our invariant aka properties
 
 /* Keep in mind
@@ -5,3 +6,14 @@
  1: The total supply of DSC should be less than the total value of collateral
  2: Getter view functions should never revert  <- evergreen invariant
 */
+
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+
+contract InvariantsTest is StdInvariant, Test {
+    
+    function setUp() external{}
+    
+}
