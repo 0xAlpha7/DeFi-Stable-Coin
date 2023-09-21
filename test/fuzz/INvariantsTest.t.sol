659896ebcca19464208e15c8fa79b999b834f24c
talha073 <talhajavedd@gmail.com>
2023-09-21 20:47:59 +0400

    fuzz testing

---
+++ test/fuzz/INvariantsTest.t.sol
@@ -0,0 +1,7 @@
+// Have our invariant aka properties
+
+/* Keep in mind
+ what are our varients
+ 1: The total supply of DSC should be less than the total value of collateral
+ 2: Getter view functions should never revert  <- evergreen invariant
+*/
