dd79c15ecf6ec0a0d7c2a49bcbaefdaf05560147
talha073 <talhajavedd@gmail.com>
2023-09-22 17:42:06 +0400

    handler invarient setup

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,1 +1,11 @@
 // Handler is going to narrow down the way we call function
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+import {Test} from "forge-std/Test.sol";
+
+
+contract Handler is Test {
+
+}
