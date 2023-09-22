242665db7d9b03dcd90a4876edfdfd210e7e6fb3
talha073 <talhajavedd@gmail.com>
2023-09-22 16:46:53 +0400

    import configs from helper config

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -15,17 +15,24 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract OpenInvariantsTest is StdInvariant, Test {
     DeployDSC deployer; 
     DSCEngine dsce; 
     DecentralizedStableCoin dsc;
     HelperConfig config;
+    address weth;
+    address wbtc;
     
     function setUp() external{
         deployer = new DeployDSC();
         (dsc, dsce, config)=deployer.run();
+        (,,weth, wbtc,) = config.activeNetworkConfig();
         targetContract(address(dsce));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTheTotalSupply() public view {
 
     }
     
