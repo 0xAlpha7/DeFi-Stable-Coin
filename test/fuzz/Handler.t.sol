6b79a40aa71f2a636fac01f46fa2dc548ef30827
talha073 <talhajavedd@gmail.com>
2023-09-23 17:23:03 +0400

    _getCollateralFromSeed function

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,10 +6,13 @@ pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract Handler is Test {
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
+    ERC20Mock weth;
+    ERC20Mock wbtc;
 
     constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc ){
         dsce = _dscEngine;
@@ -18,8 +21,16 @@ contract Handler is Test {
 
     // redeem collateral
 
-    function depositCollateral(address collateral, uint256 amountCollateral) public {
-        dsce.depositeCollateral(collateral ,amountCollateral);
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        // dsce.depositeCollateral(collateralSeed ,amountCollateral);
+    }
+
+    //Helper Function
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns(ERC20Mock) {
+        if(collateralSeed % 2 == 0){
+            return weth;
+        }
+        
     }
 
 }
