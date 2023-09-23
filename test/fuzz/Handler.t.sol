b949865f657abea67e3aee3f60e9e7d086e060f9
talha073 <talhajavedd@gmail.com>
2023-09-23 16:42:28 +0400

    constructor and import setups

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -4,8 +4,16 @@
 
 pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
-
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
 contract Handler is Test {
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc ){
+        dsce = _dscEngine;
+        dsc  = _dsc ; 
+    }
 
 }
