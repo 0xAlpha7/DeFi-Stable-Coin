d8f06d073e6d60f6d9ac1fddc8c97d35266fb125
talha073 <talhajavedd@gmail.com>
2023-09-29 14:50:34 +0400

    oracle lib

--- src/libraries/OracleLib.sol
+++ src/libraries/OracleLib.sol
@@ -2,6 +2,9 @@
 
 pragma solidity ^0.8.19;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
  /*
  * @title OracleLib
  * @notice This library is used to check the chianlink oracle for stale data
@@ -10,3 +13,22 @@ pragma solidity ^0.8.19;
  * We want DSCEngine to freez if prices becomes stale
  * so if the chainlink network explodes and you have a lot of money locked in the protocol -- to bad
  * */
+
+ library oracleLib {
+    uint256 private constant TIMEOUT = 3 hours;
+
+    function stalePriceCheck(AggregatorV3Interface priceFeed) public view returns(uint80, int256, uint256, uint256, int80) 
+    {
+      
+    (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    ) = priceFeed.latestRoundData();
+
+
+    }
+    
+ }
