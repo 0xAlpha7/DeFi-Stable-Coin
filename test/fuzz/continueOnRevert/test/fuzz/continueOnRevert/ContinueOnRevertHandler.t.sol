bcd5d04a33952becdb245f899b76c44e0284188f
talha073 <talhajavedd@gmail.com>
2023-09-30 18:41:33 +0400

    continue on revert

---
+++ test/fuzz/continueOnRevert/test/fuzz/continueOnRevert/ContinueOnRevertHandler.t.sol
@@ -0,0 +1,33 @@
+// Commented out for now until revert on fail == false per function customization is implemented
+
+// // SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+// import {Test} from "forge-std/Test.sol";
+// import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+// import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+// import {DSCEngine, AggregatorV3Interface} from "../../../src/DSCEngine.sol";
+// import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
+// import {Randomish, EnumerableSet} from "../Randomish.sol";
+// import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+// import {console} from "forge-std/console.sol";
+
+// contract ContinueOnRevertHandler is Test {
+//     using EnumerableSet for EnumerableSet.AddressSet;
+//     using Randomish for EnumerableSet.AddressSet;
+
+//     // Deployed contracts to interact with
+//     DSCEngine public dscEngine;
+//     DecentralizedStableCoin public dsc;
+//     MockV3Aggregator public ethUsdPriceFeed;
+//     MockV3Aggregator public btcUsdPriceFeed;
+//     ERC20Mock public weth;
+//     ERC20Mock public wbtc;
+
+//     // Ghost Variables
+//     uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+// }
