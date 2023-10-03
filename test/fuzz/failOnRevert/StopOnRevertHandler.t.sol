b62164c43eee292625f5a19be0062c70a840996d
talha073 <talhajavedd@gmail.com>
2023-10-03 17:36:00 +0400

    setup

---
+++ test/fuzz/failOnRevert/StopOnRevertHandler.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {Test} from "forge-std/Test.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+import {DSCEngine, AggregatorV3Interface} from "../../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
+import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+import {console} from "forge-std/console.sol";
+
+contract StopOnRevertHandler is Test {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    // Deployed contracts to interact with
+    DSCEngine public dscEngine;
+    DecentralizedStableCoin public dsc;
+    MockV3Aggregator public ethUsdPriceFeed;
+    MockV3Aggregator public btcUsdPriceFeed;
+    ERC20Mock public weth;
+    ERC20Mock public wbtc;
+
+    // Ghost Variables
+    uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dscEngine = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory collateralTokens = dscEngine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(weth)));
+        btcUsdPriceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(wbtc)));
+    }
+
+    // FUNCTOINS TO INTERACT WITH
+
+    
+}
